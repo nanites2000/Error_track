fc9e64e7371f67edf76d0b36f342f279e01fcd0e
glarsen <nanites2000@yahoo.com>
2017-11-27 13:41:22 -0700

    the Gui now works a little.  I will try to add listboxes that refence external files

--- StealthDatabase.py
+++ StealthDatabase.py
@@ -1,11 +1,17 @@
 import sqlite3
 import time
 from datetime import datetime
+from tkinter import *
+from tkinter import ttk
+import string
+
+
+
+
 
 times = datetime.now().strftime("\'%T:%f\'")
 dates= time.strftime("'%F'")
-print("date:%s"%(dates))
-print(times)
+
 
 connection = sqlite3.connect("company.db")
 
@@ -13,6 +19,7 @@ cursor = connection.cursor()
 
 # delete
 #cursor.execute("""DROP TABLE stealthErrors;""")
+
 
 
 #this creates the table if it doesn't already exist.
@@ -26,41 +33,83 @@ errorCode VARCHAR(100) ,
 errorDetail VARCHAR(300),
 note VARCHAR(600)
 );"""
-
-cursor.execute(sql_command)
-
-#sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-#    VALUES (NULL, "William", "Shakespeare", "m", "1961-10-25");"""
-#cursor.execute(sql_command)
-
-
-sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-    VALUES (NULL, "Frank", "Schiller", "m", "1955-08-17");"""
-cursor.execute(sql_command)
-detail = '"asgsgsgs"sdfsdfgsdg"sdgsdgsdg"'''
-sql_command = "INSERT INTO stealthErrors (dates,times,blenderModel,errorCode,errorDetail,note) VALUES (%s, %s, '5578', 'ctap', '%s', 'the screw broke');"%(dates,times, detail)
 cursor.execute(sql_command)
 
 
 
-cursor.execute("SELECT * FROM stealthErrors")
-print("\nfetch one:")
-res = cursor.fetchone()
-print(res)
 
-cursor.execute("SELECT * FROM stealthErrors")
-print("fetchall:")
-result = cursor.fetchall()
-for r in result:
-    print(r)
+#this will actually submit the changes to  the sql database
+def submit(*args):
 
-cursor.execute("SELECT * FROM stealthErrors WHERE blenderModel = 5578")
-print("fetchall:")
-result = cursor.fetchall()
-for r in result:
-    print(r)
 
-# never forget this, if you want the changes to be saved:
-connection.commit()
+    # sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
+    #    VALUES (NULL, "William", "Shakespeare", "m", "1961-10-25");"""
+    # cursor.execute(sql_command)
+    model= blenderModel.get()
+    detail = '"asg"'''
+    sql_command = "INSERT INTO stealthErrors (dates,times,blenderModel,errorCode,errorDetail,note) VALUES (%s, %s, '%s', 'ctap', '%s', 'the screw broke');" % (
+    dates, times, model, detail)
+    cursor.execute(sql_command)
 
-connection.close()
+    #cursor.execute("SELECT * FROM stealthErrors")
+    #print("\nfetch one:")
+    #res = cursor.fetchone()
+   # print(res)
+
+    cursor.execute("SELECT * FROM stealthErrors")
+    print("fetchall:")
+    result = cursor.fetchall()
+    for r in result:
+        print(r)
+
+
+
+    # never forget this, if you want the changes to be saved:
+    connection.commit()
+
+    #
+
+
+
+
+
+root = Tk()
+root.title("Enter Errors")
+root.geometry("1360x750")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+blenderModel = StringVar()
+meters = StringVar()
+
+modelFrame = ttk.Frame(mainframe)
+modelFrame.grid(column=3, row= 1)
+ttk.Label(modelFrame, text="Blender Model:").grid(column=0, row=1, sticky=W)
+blenderModelEntry = ttk.Entry(modelFrame, width=7, textvariable=blenderModel)
+blenderModelEntry.grid(column=1, row=1, sticky=(W))
+
+noteFrame = ttk.Frame(mainframe)
+noteFrame.grid(column=3, row= 2)
+ttk.Label(modelFrame, text="Blender Model:").grid(column=0, row=1, sticky=W)
+blenderModelEntry = ttk.Entry(modelFrame, width=7, textvariable=blenderModel)
+blenderModelEntry.grid(column=1, row=1, sticky=(W))
+
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Submit", command=submit).grid(column=3, row=3, sticky=W)
+
+
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+#feet_entry.focus()
+root.bind('<Return>', submit)
+
+root.mainloop()
+
+
